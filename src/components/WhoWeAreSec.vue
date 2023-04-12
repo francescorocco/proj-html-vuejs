6cfb14a61194c35a5de3bedb910310d01f865b9e
Francesco Rocco <francescorocco97@gmail.com>
2023-04-12 16:06:30 +0200

    section 1 completata

--- src/components/WhoWeAreSec.vue
+++ src/components/WhoWeAreSec.vue
@@ -37,12 +37,27 @@
     <section>
         <div class="container">
             <div class="img-wrapper">
+                
+                	<!-- Qua vengono gestite le 3 immagini in primo piano -->
+
                 <cardImg v-for="(element,i) in cardList" :key="i" 
                 :imgUrl="element.imgUrl"
                 :cardTitle="element.cardTitle"
                 :cardSubtitle="element.cardSubtitle"
                 ></cardImg>
-                
+            </div>
+
+            <article>"How you respond to the challenge in the second half <br> will determine what you become after the game,<br> whether you are a winner or a loser."</article>
+
+            <div class="underline"><i class="fa-solid fa-angle-up"></i><i class="fa-solid fa-angle-up"></i><i class="fa-solid fa-angle-up"></i></div>
+            
+                    <!-- Qua viene gestrita la card contenente l'autore della citazione -->
+            <div class="small-card">
+                <div class="circular-img-wrapper">
+                    <img class="circular-img" src="../assets/avatar-1-2x.jpg" alt="Brand Johnson">
+                </div>
+                <div class="name-box">Brand Johnson</div>
+                <div class="place-box">Las Vegas</div>
             </div>
         </div>
     </section>
@@ -66,5 +81,56 @@
     .img-wrapper{
         @include mixins.d-flex-space-between;
         margin-top:-50px;
+    }
+
+    article{
+        text-align: center;
+        margin-top: 150px;
+        color: var(--light-text-color);
+        font-size: 50px;
+        font-weight: 500;
+        line-height: 70px;
+    }
+
+    .underline{
+        color: var(--underline-color);
+        text-align: center;
+        margin-top: 60px;
+        font-size: 35px;
+    }
+
+    .underline i {
+        margin-right: -6px;
+    }
+
+    .circular-img-wrapper{
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        object-fit: contain;
+        
+    }
+
+    .circular-img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+    }
+
+    .small-card{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 20px;
+        font-weight: 500;
+    }
+
+    .name-box{
+        color: var(--light-text-color);
+        padding: 0 20px;
+    }
+
+    .place-box{
+        color: var(--unselected-text-color);
     }
 </style>
